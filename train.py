678d3b29704b6366ef9f8484b1956b5870e24cfc
Ileana Lim <ileanalim@mac.lan>
2026-01-28 13:27:57 -0800

    Using load_and_process_data function from preprocessing.py

--- train.py
+++ train.py
@@ -1,8 +1,13 @@
 from peft import LoraConfig, PeftModel, LoraModel, LoraConfig, get_peft_model
 from transformers import AutoModelForSpeechSeq2Seq
-model_name_or_path = "openai/whisper-tiny"
+from preprocessing import load_and_process_data
 
-model = AutoModelForSpeechSeq2Seq.from_pretrained(model_name_or_path, device_map="auto")
+dataset_path = "tiny/data.csv"
+model_name_or_path = "openai/whisper-tiny",
+
+dataset_dict, processor, data_collator = load_and_process_data(dataset_path)
+model = AutoModelForSpeechSeq2Seq.from_pretrained(model_name_or_path)
+
 model.config.forced_decoder_ids = None
 model.config.suppress_tokens = []
 config = LoraConfig(r=32, lora_alpha=64, target_modules=["q_proj", "v_proj"], lora_dropout=0.05, bias="none")
