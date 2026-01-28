ff138aa923df726c00303f3191a87fea39df78aa
Ileana Lim <ileanalim@mac.lan>
2026-01-28 12:03:11 -0800

    Made train test validate split

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,6 +7,15 @@ task = "transcribe"
 dataset_name = "tiny/data.csv"
 
 from transformers import AutoFeatureExtractor, AutoTokenizer, AutoProcessor
-from datasets import load_dataset
-dataset = load_dataset("csv", data_files=dataset_name)
-print(dataset)
+from datasets import load_dataset, DatasetDict
+raw_dataset = load_dataset("csv", data_files=dataset_name)
+
+dataset = raw_dataset["train"].shuffle(seed = 15)
+train_test = dataset.train_test_split(test_size=0.2, seed=15)
+test_valid = train_test["test"].train_test_split(test_size=0.5, seed=15)
+
+dataset_dict = DatasetDict({
+    "train": train_test["train"],
+    "validation": test_valid["train"],
+    "test": test_valid["test"],
+})
